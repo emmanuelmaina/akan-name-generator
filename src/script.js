04edba16704814d8a9f9fac16aaea6147666952e
emmanuelmaina <irunguemmanuel808@gmail.com>
2025-04-17 15:27:36 +0300

    first commit

---
+++ src/script.js
@@ -0,0 +1,33 @@
+const Birthdate = document.getElementById("Birthdate");
+const Gender = document.getElementById("Gender");
+const result = document.getElementById("result");
+
+const names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+const names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+function
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
