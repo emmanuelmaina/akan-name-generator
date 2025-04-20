698d6ce9de603865253c9fae0b3e33191c05bafb
emmanuelmaina <irunguemmanuel808@gmail.com>
2025-04-20 22:41:34 +0300

    add README.md

---
+++ README.md
@@ -0,0 +1,40 @@
+# Akan Name Generator
+
+## Description
+For our Ghananian brothers and sisters,this app helps them to generate their Akan name regardless of the day,month or year with utmost accuracy.
+
+## About Me
+My name is Emmanuel Irungu Maina.
+
+
+## Tech Start-Up
+1. Install **Visual Studio Code** (VSCode).
+2. Install **Chrome Browser**.
+3. Download the project files.
+4. Open the files using **VSCode**, which will be displayed in Chrome Browser.
+
+## Behaviour Driven Development
+1. Enter your date of birth correctly.
+2. If you put a wrong date or fail to choose a gender it displays an alert.
+3. The Akan name is generated accurately
+
+## Technologies used
+1. HTML
+2. CSS
+3. JS
+
+## Contact Information
+Phone No. : 0724282539
+Email     : irunguemmanuel808@gmail.com
+Instagram : manu_irush
+
+## License and copyright informtion
+MIT License © 2025 Emmanuel Irungu Maina
+
+
+
+
+
+
+
+
--- src/script.js
+++ src/script.js
@@ -1,33 +1,71 @@
-const Birthdate = document.getElementById("Birthdate");
-const Gender = document.getElementById("Gender");
+/*const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+const gender = document.getElementById("gender");
 const result = document.getElementById("result");
 
-const names = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
-const names = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-
-function
-
-
-
-
-
-
-
-
+function generate() {
+    if (month = 1 , 3, 5, 7, 8, 10, 12){
+        day === 
+    }
+        
+}
+    */
+const birthDate = document.getElementById("birthDate");
+const gender = document.getElementById("gender");
+const result = document.getElementById("result");
 
 
+const form = document.getElementById("form")
+form.addEventListener("submit",function(event){
+    event.preventDefault()
+
+    
+     const birthDate = document.getElementById("birthDate").value;
+     const [year, month, day] = birthDate.toString().split("-").map(Number)
+    
+     let CC = Math.floor(year / 100);
+     let YY = year % 100;
+     let MM = month;
+     let DD = day;
+     let d = Math.floor((((CC / 4) - 2 * CC - 1) + ((5 * YY) / 4) + ((26 * (MM + 1)) / 10) + DD) % 7);
+     
+     
+     
+
+} )
+/*
+console.log(d)
+let form = document.getElementById("form");
+const female = document.getElementById("female");
+const gender = document.getElementById("gender");
+const result = document.getElementById("result");
+const birthdate = document.getElementById("birthdate").value;
+
+function generate(birthdate) {
+    
+    const birthDate = document.getElementById("birthDate");
+    const [year, month, day] = birthDate.split("-").map(Number)
+    let CC = Math.floor(year / 100);
+    let YY = year % 100;
+    let MM = month;
+    let DD = day;
+    let d = Math.floor((((CC / 4) - 2 * CC - 1) + ((5 * YY) / 4) + ((26 * (MM + 1)) / 10) + DD) % 7);
+    console.log(d)
+    return d;
+}
+
+
+const birthdate = document.getElementById("birthdate").value;
+const[year, month, day ]= birthdate.split("-").map(Number) 
 
 
 
+const Female = document.getElementById("Female");
+const Male = document.getElementById("Male");
+const result = document.getElementById("result");
 
+const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]
+const FemaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 
-
-
-
-
-
-
-
-
-
-
+*/
